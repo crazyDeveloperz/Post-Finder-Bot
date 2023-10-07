b21428bcb21f6e4698fd87028e7a3abc52bfaa72
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-10-07 05:46:45 -0700

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 25069425
 API_HASH     = "41034e257e6449615faea5f18bbe1dd7"
 BOT_TOKEN    = "6159314541:AAFWdl3qSyNUjpwKPvpjG39PxXRpEOmL7t8"
 SESSION      = "BQGjmq4ANfqnpXdIiwhzOME3SR02ehigLdWjE-bvtE4AooJkgXuot9cQd8GsOuJPNyFXQwQtMwMok17TkDgyuE6LKnwez9tRllZul3-1NaF68kpD0dVnr5RjFEKB_A3dAm8oeHXuKmEkfZiYor464uplR8yanu5_butEis_JssCdqqEh4_9jxibcDVtPLinnDoFc4Oa4gX9iBOU-vIykca--2pEKS5w34LULdn8fkcQGbZBK-lx_QI0FC4-lChGbP53FVkAwDtOdxyfhnfyGF-V4j5RWW0ukFkTUF1v1clLCfQ4xNS4NEUPALPFDFNspC6kfhicTGlIIWO6i2_EfZsMocQePGwAAAAFlbcqHAA"
-DATABASE_URI = "mongodb+srv://hegodal811:ehNXCGMvCqxKj1w3@cluster0.3xnorax.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://pOSTsEARCHbOT:pOSTsEARCHbOT@cluster0.jjbjn95.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001876507111
 ADMIN        = 6249148586
 CHANNEL      = "@Crazybotz"
